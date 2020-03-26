d7c7e7bbfe9f89d63302fc5b3ac76376f3ffeefa
Mike Chadwick <mikechadwick@Mikes-MacBook-Pro.local>
2020-03-25 23:38:54 -0600

    react is super hard, want help on understanding #7

--- app-7/src/App.js
+++ app-7/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import NewTask from './NewTask'
+import List from './List'
 
-function App() {
+class App extends Component{
+  constructor(){
+    super()
+    this.state={
+      list:[] //stored in a var called "tasks" passed to child List.js folder
+    }
+    //this is what allows us to pass our "handleAddTask" method to our child NewTask.js folder in the form of a variable "add" that is declaired in the <NewTask/>
+    this.handleAddTask = this.handleAddTask.bind(this)
+  }
+  handleAddTask(task){
+    this.setState({list: [...this.state.list, task]}) //need clarity here, what does this line do?
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My to-do List:</h1>
+     <NewTask add={this.handleAddTask}/>
+     <List tasks={this.state.list}/>
     </div>
   );
+}
 }
 
 export default App;
